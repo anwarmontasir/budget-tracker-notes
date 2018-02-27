3f6170330c1f0932f9a3d6ae20098645c7959e7d
Anwar Montasir <anwarmontasir@gmail.com>
2018-02-27 14:56:22 -0800

    finish form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,12 +8,16 @@ export default class Contact extends Component {
         <div>
           <h1>Contact form</h1>
           <p>Make it accessible, or <a href="https://github.com/evcohen/eslint-plugin-jsx-a11y" target="_blank" rel="noopener noreferrer">eslint-plugin-jsx-a11y</a> will warn you!</p>
+          <p>I didn't style this form yet :(</p>
           <form>
             <fieldset>
               <legend>Your details</legend>
               <label htmlFor="userName">Name:
-              <input id="userName" type="text" name="name"/> </label>
+              <input id="userName" type="text" name="name" /></label>
+              <label htmlFor="userEmail">Email:
+              <input id="userEmail" type="email" name="email" /></label>
             </fieldset>
+            <button type="submit">Submit</button>
           </form>
         </div>
       </DocumentTitle>
